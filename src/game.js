18fae0812886139ea898b0ee5ff15074e6238d16
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-02-05 18:02:24 +0530

    places the symbols in the middle of the grid
    * after creating the grid , which is given to the placeSymbols function* which places the symbols

--- src/game.js
+++ src/game.js
@@ -1,6 +1,16 @@
 export const createGrid = (rows, cols) => {
   return Array.from(
     { length: rows },
-    () => Array.from({ length: cols }, () => ""),
+    () => Array.from({ length: cols }, () => " "),
   );
+};
+
+export const placeSymbolsIntoGrid = (grid, sym1, sym2) => {
+  const index1 = grid.length / 2;
+  const index2 = grid[0].length / 2;
+  grid[index1 - 1][index2 - 1] = sym1;
+  grid[index1 - 1][index2] = sym2;
+  grid[index1][index2 - 1] = sym2;
+  grid[index1][index1] = sym1;
+  return grid;
 };
--- test/game_test.js
+++ test/game_test.js
@@ -1,13 +1,28 @@
 import { assertEquals } from "@std/assert";
 import { describe, it } from "jsr:@std/testing/bdd";
-import { createGrid } from "../src/game.js";
+import { createGrid, placeSymbolsIntoGrid } from "../src/game.js";
 
 describe("othella game", () => {
   describe("testing the grid create functionality : ", () => {
     it("2:2 grid is creating : ", () => {
       const actual = createGrid(2, 2);
-      const expected = [["", ""], ["", ""]];
+      const expected = [[" ", " "], [" ", " "]];
       assertEquals(actual, expected);
     });
   });
+
+  describe("placing the symbols in the middle : ", () => {
+    it("placing the symbols in the middle of the grid : ", () => {
+      const grid = createGrid(4, 4);
+      const actual = placeSymbolsIntoGrid(grid, '1', '0');
+      const expected = [
+        [" ", " ", " ", " "],
+        [" ", "1", "0", " "],
+        [" ", "0", "1", " "],
+        [" ", " ", " ", " "],
+      ];
+      assertEquals(actual, expected);
+    });
+  });
+
 });
